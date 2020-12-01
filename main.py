cc4ef5e849f80150cc193451bca9a4d3b7ec7059
kirillpyl <75266793+kirillpyl@users.noreply.github.com>
2020-12-01 15:28:34 +0300

    first

--- main.py
+++ main.py
@@ -3,14 +3,51 @@ from random import randint
 
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import uic
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Example(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(600, 600)
+        MainWindow.setMinimumSize(QtCore.QSize(600, 600))
+        MainWindow.setMaximumSize(QtCore.QSize(600, 600))
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.draw = QtWidgets.QPushButton(self.centralwidget)
+        self.draw.setGeometry(QtCore.QRect(200, 510, 191, 51))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setUnderline(False)
+        font.setWeight(75)
+        font.setStrikeOut(False)
+        font.setKerning(True)
+        font.setStyleStrategy(QtGui.QFont.PreferDefault)
+        self.draw.setFont(font)
+        self.draw.setMouseTracking(False)
+        self.draw.setObjectName("draw")
+        self.circle = QtWidgets.QLabel(self.centralwidget)
+        self.circle.setGeometry(QtCore.QRect(20, 20, 551, 461))
+        self.circle.setText("")
+        self.circle.setObjectName("circle")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.draw.setText(_translate("MainWindow", "DRAW"))
+
+
+class Example(Ui_MainWindow, QMainWindow):
     def __init__(self):
         self.x = 0
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.draw.clicked.connect(self.run)
 
     def paintEvent(self, event):
@@ -21,7 +58,7 @@ class Example(QMainWindow):
         qp.end()
 
     def dr(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         t = randint(1, 200)
         qp.drawEllipse(230, 130, t, t)
 
